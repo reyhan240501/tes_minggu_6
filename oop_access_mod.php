cec3ae78b4fdea715b4e9373a96e70d4729841dc
muhammad reyhan rizki ferdiyan <70565254+reyhan240501@users.noreply.github.com>
2020-10-03 23:45:48 +0700

    finnish

---
+++ oop_access_mod.php
@@ -0,0 +1,25 @@
+<?php
+
+/** 
+ * 
+ * poin 2
+ * 
+ * - 1 || Buatlah sebuah class dengan menginplementasikan access modifier pada method maupun properties
+ * - 1 || invoke/call method yang kalian buat dengan visibilitas static maupun non static
+ */
+
+class accesMod
+{
+    public $name;
+
+    public function setName($nama)
+    {
+        $this->name=$nama;
+    }
+}
+
+$oop = new accesMod();
+$oop->setName("Muhammad Reyhan Rizki Ferdiyan");
+echo $oop->name;
+
+?>
